058e9eacbc8bf6ab8174bfe61aa4b67d77270d42
Developersokkrey <72006616+Developersokkrey@users.noreply.github.com>
2023-10-16 16:37:56 +0700

    Update ADO.NET.cs

--- ADO.NET.cs
+++ ADO.NET.cs
@@ -37,4 +37,56 @@ public async Task<List<Branch>> GetAllBranch()
         //}
         return branch;
     }
+    --------------------------------------------------------------------------------------------------
+    public IEnumerable<object> GetAllBranchAsEnumerable()
+{
+    //return new List<T>();
+    DataSet ds = new DataSet();
+    List<Branch> branches = new List<Branch>();
+    using (SqlConnection con = new SqlConnection(_connectionString))
+    {
+        string query = "SELECT * FROM GetAll_Branches()";
+        using (SqlCommand cmd = new SqlCommand(query))
+        {
+            cmd.Connection = con;
+            using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+            {
+                sda.Fill(ds);
+            }
+        }
+        var branch = ds.Tables[0].AsEnumerable()
+        .Select(dataRow => new Branch
+        {
+            Name = (string)dataRow["Name"],
+            Code = (string)dataRow["Code"]
+            //Name = dataRow.Field<string>("Name"),
+            //Code = dataRow.Field<string>("Code")
+        }).ToList();
+        //foreach (DataRow dr in ds.Tables[0].Rows)
+        //{
+        //    branches.Add(new Branch
+        //    {
+        //        BranchID = (ulong)Convert.ToInt32(dr["BranchID"]),
+        //        Name = Convert.ToString(dr["Name"]),
+        //        Code = Convert.ToString(dr["Code"]),
+        //        Address = Convert.ToString(dr["Address"]),
+        //        Location = Convert.ToString(dr["Location"]),
+        //        CompID = (ulong)Convert.ToInt32(dr["CompID"])
+        //    });
+        //}
+        return branch;
+    }
+    -------------------------------------------
+    public async Task<IActionResult> GetAllBranchesAsync()
+{
+    try
+    {
+        var branches =(IEnumerable<Branch>) _branchesRepo.GetAllBranchAsEnumerable();
+        return Ok(branches);
+    }
+    catch 
+    {
+        throw;
+    }
+}
   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
